a5d585857f90f8212e727621a773d54c12d7cef1
Juliana Melo <136100085+julianafhm@users.noreply.github.com>
2024-07-05 09:11:03 -0300

    Delete javascript DOM/Selecionando elemento 04 directory

--- javascript DOM/Selecionando elemento 04/script.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const p = document.getElementsByClassName ("paragrafo");
-console.log(p);
-
-
-for (let element of p){  // foreach n funciona pq nao é Nodelist
-    console.log(element);
-}
