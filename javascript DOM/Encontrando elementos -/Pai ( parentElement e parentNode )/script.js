5056dc7c7802aac3b9b3b2ac456638cae5b06e38
Juliana Melo <136100085+julianafhm@users.noreply.github.com>
2024-07-08 16:18:06 -0300

    Delete javascript DOM/Encontrando elementos -/Pai ( parentElement e parentNode ) directory

--- javascript DOM/Encontrando elementos -/Pai ( parentElement e parentNode )/script.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const strong = document.querySelector("strong");
-const p = document.querySelector(".paragrafo");
-const container = document.querySelector(".container");
-const body = document.querySelector("body");
-
-console.log(body.parentElement);
-console.log(body.parentNode);
